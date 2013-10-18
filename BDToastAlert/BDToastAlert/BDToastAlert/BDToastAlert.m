a0a1f9d62a21986bd80e90d84e14bc83f06e4395
nicolasmuller <nicolas@beingenious.com>
2013-10-18 12:00:06 +0200

    fix width padding

--- BDToastAlert/BDToastAlert/BDToastAlert/BDToastAlert.m
+++ BDToastAlert/BDToastAlert/BDToastAlert/BDToastAlert.m
@@ -163,7 +163,7 @@
     }else if(self.verticalAlignment == BDToastVerticalAlignmentTop){
         toastView.center = CGPointMake(centerOfView.x, kMargin);
     }else {
-        toastView.center = CGPointMake(centerOfView.x - (CGRectGetWidth(toastView.bounds) / 2), CGRectGetHeight(viewToShowOn.bounds) - CGRectGetHeight(toastView.bounds) - (_isKeyboardShowing?kHeightKeyboard:0));
+        toastView.center = CGPointMake(centerOfView.x, CGRectGetHeight(viewToShowOn.bounds) - CGRectGetHeight(toastView.bounds) - (_isKeyboardShowing?kHeightKeyboard:0));
     }
 
     toastView.frame = CGRectIntegral(toastView.frame);
